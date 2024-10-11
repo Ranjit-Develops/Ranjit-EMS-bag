f3a984b17b73200f8d8a0eca188de5ed0ec5cfe4
Trijal Gandotra <102646493+Ranjit-Develops@users.noreply.github.com>
2024-10-11 13:50:06 +0530

    Update Money deduction

--- configs/config.lua
+++ configs/config.lua
@@ -5,6 +5,8 @@ Config = {}
 
 Config.Bag = {
 	Job = "ambulance",
+	Money = 500, --Amount to deduct;set to False to disable ; disable means not even 0 will be deducted 
+
 }
 Config.Stash = {
 	MaxWeighStash = 50000,
